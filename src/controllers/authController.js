78c7625502a4e344a7b6ddd95383ef987d90fb46
DoubleFelipe <felipemouraroieski@gmail.com>
2026-05-04 22:08:30 -0300

    Model de Usuario e Autenticação de login v1.0
    Foi feito os arquivos de model e autenticação de login

---
+++ src/controllers/authController.js
@@ -0,0 +1,33 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const hash = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hash
+  });
+
+  res.json(user);
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) return res.status(404).json({ erro: "Usuário não encontrado" });
+
+  const valid = await bcrypt.compare(password, user.password);
+
+  if (!valid) return res.status(401).json({ erro: "Senha inválida" });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+  res.json({ token });
+};
